4f5946128086e9cfd6a7e7c8a5d89bde649221fc
brijnandan <bpruipjy@gmail.com>
2017-07-10 08:25:45 +0530

    added change for taking user inputswq

--- git/convert.sh
+++ git/convert.sh
@@ -3,9 +3,25 @@ if  [ "$#" -eq 0 ] || [ "$#" -eq 1 ] ||  [ "$#" -gt 2 ]
 	{
 	echo " The usage of the script is not correct "
 	echo " There should be exactly two command line arg"
-	echo " First will be source and second destinaltion directory.."
+	echo " First will be source and second destination directory.."
 	echo " Usage ./convert.sh source_path destination_path.."
 	}
 else 
 	echo "test"
+fi
+
+
+
+source_path=$1
+destination_path=$2
+src=$(echo $1 | cut -c1)
+des=$(echo $2 | cut -c1)
+
+if  [ "$src" = "/" ] && [ "$des" = "/"  ]
+	then
+	{
+	echo " the directories are perfect, You can proceed with the conversion   " 
+	}
+	else
+	echo " directories are not perfect please provide full path from root"
 fi
